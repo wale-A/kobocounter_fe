e31fadbec68eb8dae967bd85130ab5ceb447c69c
wale A <isiaq.oa@gmail.com>
2021-06-11 18:53:13 +0100

    refactor: Adding a fetch event handler

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,5 @@
+const staticCacheName = "kobocounter-cache-v1";
+
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 
 self.addEventListener("push", function (e) {
@@ -13,5 +15,48 @@ self.addEventListener("push", function (e) {
       data.title || "Notified by KoboCounter",
       options
     )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        if (response) {
+          // console.log("Found ", event.request.url, " in cache");
+          return response;
+        }
+        // console.log("Network request for ", event.request.url);
+        return fetch(event.request).then((response) => {
+          // TODO 5 - Respond with custom 404 page
+
+          return caches.open(staticCacheName).then((cache) => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+      })
+      .catch((error) => {
+        // TODO 6 - Respond with custom offline page
+      })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Activating new service worker...");
+
+  const cacheAllowlist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
