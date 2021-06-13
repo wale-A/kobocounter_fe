9883c758cdfe0b4c185c015c68815362dff0e2e4
wale A <isiaq.oa@gmail.com>
2021-06-13 07:05:37 +0100

    refactor: Modify request fetch process

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,6 @@
 const staticCacheName = "kobocounter-cache-v1";
 const OFFLINE_URL = "/404";
+const cacheAllowlist = [staticCacheName];
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 
@@ -18,7 +19,7 @@ self.addEventListener("push", function (e) {
   const data = e.data.json();
   const options = {
     body: data.body,
-    icon: `../public/favicon.ico`,
+    icon: `https://kobocounter.com/img/icons/android/android-launchericon-48-48.png`,
     image: `https://kobocounter.com/img/icons/android/android-launchericon-48-48.png`,
     vibrate: [100, 50, 100],
   };
@@ -32,41 +33,35 @@ self.addEventListener("push", function (e) {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
+    fetch(event.request)
       .then((response) => {
-        if (response) {
-          // console.log("Found ", event.request.url, " in cache");
-          return response;
-        }
-        // console.log("Network request for ", event.request.url);
-        return fetch(event.request).then((response) => {
-          // TODO 5 - Respond with custom 404 page
-
+        if (response.status === 404) {
+          return caches.match(event.request).then((cacheResponse) => {
+            if (cacheResponse) {
+              return cacheResponse;
+            }
+          });
+        } else {
           return caches.open(staticCacheName).then((cache) => {
             cache.put(event.request.url, response.clone());
             return response;
           });
-        });
+        }
       })
       .catch((error) => {
-        return caches.open(staticCacheName).then((cache) => {
-          return cache.then((offlineResponse) => offlineResponse);
-        });
+        // return caches.open(staticCacheName).then((cache) => {
+        //   return cache.then((offlineResponse) => offlineResponse);
+        // });
       })
   );
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Activating new service worker...");
-
-  const cacheAllowlist = [staticCacheName];
-
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheAllowlist.indexOf(cacheName) === -1) {
+          if (cacheAllowlist.indexOf(cacheName) !== -1) {
             return caches.delete(cacheName);
           }
         })
